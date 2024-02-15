1d0ebe8d39030e919fc5d542450932172fc562de
Your Name <you@example.com>
2024-02-15 10:17:04 +0100

    Resourcen Page 2

---
+++ src/test/java/pageObjects/DispositionPage.java
@@ -0,0 +1,58 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class DispositionPage {
+    private final WebDriver webDriver;
+    private final By plz = By.id("postcode-input-id");
+    private final By ort = By.id("city-input-id");
+    private final By strasse = By.id("street-name-input-id");
+    private final By hausNummmer = By.id("street-nr-first-input-id");
+    private final By plannungsGruppe = By.id("action-planning-group-input-id");
+    private final By poolButton = By.xpath("//div[@id='navigation_header-pool-id']//button");
+
+    public DispositionPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    public void fillDisposition(String newPlzValue, String newOrtValue, String newStrasseValue,
+                                String newHausNummerValue, String newplannungsGruppeValue) {
+        setPlz(newPlzValue);
+        setOrt(newOrtValue);
+        setStrasse(newStrasseValue);
+        setHausNummmer(newHausNummerValue);
+        setplannungsGruppe(newplannungsGruppeValue);
+    }
+
+    public void setPlz(String newPlzValue) {
+        changeField(webDriver.findElement(this.plz), newPlzValue);
+    }
+
+    public void setOrt(String newOrtValue) {
+        changeField(webDriver.findElement(this.ort), newOrtValue);
+    }
+
+    public void setStrasse(String newStrasseValue) {
+        changeField(webDriver.findElement(this.strasse), newStrasseValue);
+    }
+
+    public void setHausNummmer(String newHausNummerValue) {
+        changeField(webDriver.findElement(this.hausNummmer), newHausNummerValue);
+    }
+
+    public void setplannungsGruppe(String newplannungsGruppeValue) {
+        changeField(webDriver.findElement(this.plannungsGruppe), newplannungsGruppeValue);
+    }
+
+    public void goNextPage() {
+        webDriver.findElement(this.poolButton).click();
+    }
+
+    private void changeField(WebElement element, String newValue) {
+        element.clear();
+        element.sendKeys(newValue);
+    }
+
+}
--- src/test/java/pageObjects/RessourcenPage.java
+++ src/test/java/pageObjects/RessourcenPage.java
@@ -1,4 +1,12 @@
 package pageObjects;
 
 public class RessourcenPage {
+
+
+  Delete //*[@id="delete-employees"]
+    Area label Anna //*[@id="single-spa-application:@ad-portal/workforce-resource-local"]/div/div/div[1]/div/div[2]/div[2]/div/div/div/div[1]/span/input
+    aria-label ="Select row"
+    type= button, id= btn-confirm-delete
+
+
 }
