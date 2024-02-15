09ef550a983fac2fbec235a21679a29ca27f21f8
Your Name <you@example.com>
2024-02-15 10:28:16 +0100

    Ressourcen Page new

--- src/test/java/pageObjects/RessourcenPage.java
+++ src/test/java/pageObjects/RessourcenPage.java
@@ -3,10 +3,10 @@ package pageObjects;
 public class RessourcenPage {
 
 
-  Delete //*[@id="delete-employees"]
-    Area label Anna //*[@id="single-spa-application:@ad-portal/workforce-resource-local"]/div/div/div[1]/div/div[2]/div[2]/div/div/div/div[1]/span/input
-    aria-label ="Select row"
-    type= button, id= btn-confirm-delete
-
+//  Delete //*[@id="delete-employees"]
+//    Area label Anna //*[@id="single-spa-application:@ad-portal/workforce-resource-local"]/div/div/div[1]/div/div[2]/div[2]/div/div/div/div[1]/span/input
+//    aria-label ="Select row"
+//    type= button, id= btn-confirm-delete
+//
 
 }
